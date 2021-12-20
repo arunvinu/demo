1f19795f2063b1396f8284655ba7a506597a62e9
arunvinu <arunkumarxworkz@gmail.com>
2021-12-20 17:06:53 +0545

    add bag.java

---
+++ Bag.java
@@ -0,0 +1,13 @@
+class Bag{
+static int a = 10;     // Static variable
+int b = 20;              //instance variable
+public static void main (String args[ ]){
+
+int c = 30;              //local variable
+Bag ref = new Bag ( );
+System.out.println(a);
+System.out.println(Bag.b);
+System.out.println(c);
+}
+}
+
